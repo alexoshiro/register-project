d4f0d22aa60d1ad1489bf53743ac7c51184a7f97
Alex Oshiro <alex.oshiro94@gmail.com>
2020-02-04 00:58:51 -0400

    Added DirtiesContext to tests class to avoid stuck process embed mongodb running in host.

--- back-end/register-api/src/test/java/project/alexoshiro/registerapi/controller/PersonControllerTest.java
+++ back-end/register-api/src/test/java/project/alexoshiro/registerapi/controller/PersonControllerTest.java
@@ -1,7 +1,11 @@
 package project.alexoshiro.registerapi.controller;
 
-import static org.hamcrest.Matchers.*;
-import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.*;
+import static org.hamcrest.Matchers.hasSize;
+import static org.hamcrest.Matchers.is;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.delete;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.patch;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.post;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.jsonPath;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
 
@@ -10,13 +14,26 @@ import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.TestInstance;
 import org.junit.jupiter.api.TestInstance.Lifecycle;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
+import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.http.MediaType;
+import org.springframework.test.annotation.DirtiesContext;
+import org.springframework.test.annotation.DirtiesContext.ClassMode;
+import org.springframework.test.context.ActiveProfiles;
+import org.springframework.test.web.servlet.MockMvc;
 import org.springframework.test.web.servlet.MvcResult;
 
 import project.alexoshiro.registerapi.service.IPersonService;
 
+@DirtiesContext(classMode = ClassMode.AFTER_CLASS)
+@SpringBootTest
+@ActiveProfiles("test")
+@AutoConfigureMockMvc
 @TestInstance(Lifecycle.PER_CLASS)
-public class PersonControllerTest extends BaseController {
+public class PersonControllerTest {
+
+	@Autowired
+	protected MockMvc mockMvc;
 
 	private String token = "";
 
